efce89ed9dbd971c9dab9291c04d046d211735d4
John Weldon <john.weldon117@gmail.com>
2021-05-05 00:02:25 -0400

    unit tests updated to align with 5/5/2021 major update

--- test_ctDescStats.py
+++ test_ctDescStats.py
@@ -7,6 +7,7 @@ https://docs.python.org/3/library/unittest.html#unittest.TestCase.debug"""
 
 # Standard Library Imports
 import unittest
+
 # Third-Party Imports
 from ctDescStats import DescriptiveStats as ds
 
@@ -16,41 +17,42 @@ class TestDescrtiptiveStatsClass(unittest.TestCase):
     @classmethod
     def setUpClass(cls):
         cls.data_simple = ds((1, 2, 3, 4, 4, 5, 6, 10))
-        cls.data_negatives = ds((-1, -2, -3, -4, -4, -5, -6, -10), cl=0.98)
-        cls.data_with_zeroes = ds((1, 2, 3, 4, 4, 5, 6, 10, 0, 0, 0, 0, 0), cl=0.99)
+        cls.data_negatives = ds((-1, -2, -3, -4, -4, -5, -6, -10),
+                                cl=0.90)
+        cls.data_population_zeroes = ds((1, 2, 3, 4, 4, 5, 6, 10, 0, 0, 0, 0, 0),
+                                        cl=0.99,
+                                        is_population=True)
 
     def test_data_validations(self):
-        self.assertEqual(self.data_simple.n, 8)
-        self.assertEqual(self.data_with_zeroes.n, 13)
-        self.assertEqual(self.data_simple.df, 7)
-        self.assertEqual(self.data_with_zeroes.df, 12)
         with self.assertRaises(AssertionError):
-            ds((1, 2, 3), cl=0.97)
+            ds((1, 2, 3), cl=0.98)
             ds((1, 2, 3), cl="spaghetti")
+            ds((1, 2, 3), tails="spaghetti")
             ds((1, 2, 3), data_name=7)
             ds((1, 2, 3), data_name=True)
             ds((1, 2, 3), data_name=6.022)
 
-    def test_t_score_calculate(self):
-        self.assertEqual(self.data_simple.t_score_calculate(), 2.365)
-        self.assertEqual(self.data_negatives.t_score_calculate(), 2.988)
-        self.assertEqual(self.data_with_zeroes.t_score_calculate(), 3.055)
+    def test_get_lookup_df(self):
+        self.assertEqual(self.data_simple.n, 8)
+        self.assertEqual(self.data_population_zeroes.n, 13)
+        self.assertEqual(self.data_simple.df_lookup, 7)
+        self.assertEqual(self.data_population_zeroes.df_lookup, 999)
 
-    def test_mean_calculate(self):
-        self.assertEqual(self.data_simple.mean_calculate(), 4.375)
-        self.assertEqual(self.data_negatives.mean_calculate(), -4.375)
-        self.assertEqual(round(self.data_with_zeroes.mean_calculate(), 4), 2.6923)
+    def test_mean_calculation(self):
+        self.assertEqual(self.data_simple.mean, 4.375)
+        self.assertEqual(self.data_negatives.mean, -4.375)
+        self.assertEqual(round(self.data_population_zeroes.mean, 4), 2.6923)
 
-    def test_median_calculate(self):
-        self.assertEqual(self.data_simple.median_calculate(), 4)
-        self.assertEqual(self.data_negatives.median_calculate(), -4)
-        self.assertEqual(self.data_with_zeroes.median_calculate(), 2)
+    def test_median_calculation(self):
+        self.assertEqual(self.data_simple.median, 4)
+        self.assertEqual(self.data_negatives.median, -4)
+        self.assertEqual(self.data_population_zeroes.median, 2)
 
-    def test_mode_calculate(self):
-        self.assertEqual(self.data_simple.mode_calculate(), (4,))
-        self.assertEqual(self.data_negatives.mode_calculate(), (-4,))
-        self.assertEqual(self.data_with_zeroes.mode_calculate(), (0,))
-        self.assertEqual(ds((1, 1, 2, 2)).mode_calculate(), (1, 2))
+    def test_mode_calculation(self):
+        self.assertEqual(self.data_simple.mode, (4,))
+        self.assertEqual(self.data_negatives.mode, (-4,))
+        self.assertEqual(self.data_population_zeroes.mode, (0,))
+        self.assertEqual(ds((1, 1, 2, 2)).mode, (1, 2))
 
     def test_min_max_and_range(self):
         self.assertEqual((self.data_simple.min,
@@ -59,19 +61,57 @@ class TestDescrtiptiveStatsClass(unittest.TestCase):
         self.assertEqual((self.data_negatives.min,
                           self.data_negatives.max,
                           self.data_negatives.range), (-10, -1, 9))
-        self.assertEqual((self.data_with_zeroes.min,
-                          self.data_with_zeroes.max,
-                          self.data_with_zeroes.range), (0, 10, 10))
+        self.assertEqual((self.data_population_zeroes.min,
+                          self.data_population_zeroes.max,
+                          self.data_population_zeroes.range), (0, 10, 10))
 
-    def test_variance_calculate(self):
-        self.assertEqual(self.data_simple.variance_calculate(), 7.696)
-        self.assertEqual(self.data_negatives.variance_calculate(), 7.696)
-        self.assertEqual(self.data_with_zeroes.variance_calculate(), 9.397)
+    def test_variance_calculation(self):
+        self.assertEqual(round(self.data_simple.var, 4), 7.6964)
+        self.assertEqual(round(self.data_negatives.var, 4), 7.6964)
+        self.assertEqual(round(self.data_population_zeroes.var, 4), 8.6746)
 
-    def test_standard_deviation_calculate(self):
-        self.assertEqual(self.data_simple.standard_deviation_calculate(), 2.774)
+    def test_standard_deviation_calculation(self):
+        self.assertEqual(round(self.data_simple.stdev, 4), 2.7742)
+        self.assertEqual(round(self.data_negatives.stdev, 4), 2.7742)
+        self.assertEqual(round(self.data_population_zeroes.stdev, 4), 2.9453)
+
+    def test_standard_error_calculation(self):
+        self.assertEqual(round(self.data_simple.sterr, 4), 0.9808)
+        self.assertEqual(round(self.data_negatives.sterr, 4), 0.9808)
+        self.assertEqual(round(self.data_population_zeroes.sterr, 4), 0.8169)
+
+    def test_coeff_of_var_calculation(self):
+        self.assertEqual(round(self.data_simple.cv, 4), 0.6341)
+        self.assertEqual(round(self.data_negatives.cv, 4), -0.6341)
+        self.assertEqual(round(self.data_population_zeroes.cv, 4), 1.094)
+
+    def test_skewness_calculation(self):
+        self.assertEqual(round(self.data_simple.skew, 4), 0.7492)
+        self.assertEqual(round(self.data_negatives.skew, 4), -0.7492)
+        self.assertEqual(round(self.data_population_zeroes.skew, 4), 1.0251)
+
+    def test_score_calculations(self):
+        self.assertEqual((self.data_simple.score_type,
+                          self.data_simple.score), ("t", 2.365))
+        self.assertEqual((self.data_negatives.score_type,
+                          self.data_negatives.score), ("t", 1.895))
+        self.assertEqual((self.data_population_zeroes.score_type,
+                          self.data_population_zeroes.score), ("z", 2.576))
+
+    def test_confidence_interval_calculation(self):
+        self.assertEqual(  # data_simple
+            (round(self.data_simple.moe, 4),
+             round(self.data_simple.ci[0], 4),
+             round(self.data_simple.ci[1], 4)), (2.3197, 2.0553, 6.6947))
+        self.assertEqual(  # data_negatives
+            (round(self.data_negatives.moe, 4),
+             round(self.data_negatives.ci[0], 4),
+             round(self.data_negatives.ci[1], 4)), (1.8587, -6.2337, -2.5163))
+        self.assertEqual(  # data_population_zeroes
+            (round(self.data_population_zeroes.moe, 4),
+             round(self.data_population_zeroes.ci[0], 4),
+             round(self.data_population_zeroes.ci[1], 4)), (2.1043, 0.5881, 4.7966))
 
 
 if __name__ == "__main__":
-    ds((1, 2, 3, 4, 4, 5, 6, 10)).t_score_calculate()
     unittest.main()
