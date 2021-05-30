cd0bb921ecd41fed5e0f873a62efd74084e0354a
John Weldon <john.weldon117@gmail.com>
2021-05-29 22:43:23 -0400

    fixed setup.py, added long_description_content_type='text/markdown'

--- setup.py
+++ setup.py
@@ -13,10 +13,11 @@ setup(
   license='MIT',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
   description='A small statistics package for data science students and enthusiasts',   # Give a short description about your library
   long_description=long_description,
+  long_description_content_type='text/markdown',
   author='John Weldon',                   # Type in your name
   author_email='john.weldon117@gmail.com',      # Type in your E-Mail
   url='https://github.com/chumbie/statbasket',   # Provide either the link to your github or to your website
-  download_url='https://github.com/chumbie/statbasket/archive/refs/tags/v_01.0.02.tar.gz',    # I explain this later on
+  download_url='https://github.com/chumbie/statbasket/archive/refs/tags/v_01.0.03.tar.gz',    # I explain this later on
   keywords=[
       'statistics', 
       'data-science', 
