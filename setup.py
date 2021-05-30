e07a2b6fb249069f864b7224d130d861d6d1c1a7
John Weldon <john.weldon117@gmail.com>
2021-05-29 21:22:05 -0400

    moved main modules to src folder, added setup.py

---
+++ setup.py
@@ -0,0 +1,27 @@
+from distutils.core import setup
+setup(
+  name='statbasket',         # How you named your package folder (MyLib)
+  packages=['statbasket'],   # Chose the same as "name"
+  version='1.0',      # Start with a small number and increase it with every change you make
+  license='MIT',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
+  description='A small statstics package for data science students and enthusiasts',   # Give a short description about your library
+  author='John Weldon',                   # Type in your name
+  author_email='john.weldon117@gmail.com',      # Type in your E-Mail
+  url='https://github.com/chumbie/statbasket',   # Provide either the link to your github or to your website
+  download_url='https://github.com/chumbie/statbasket/archive/v_01.tar.gz',    # I explain this later on
+  keywords=[
+      'statistics', 
+      'data-science', 
+      'students', 
+      'lite', 
+      'pure-python'],   # Keywords that define your package best
+  install_requires=[],           # I get to this in a second
+      
+  classifiers=[
+    'Development Status :: 5 - Production/Stable',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
+    'Intended Audience :: Developers',      # Define that your audience are developers
+    'Topic :: Software Development :: Build Tools',
+    'License :: OSI Approved :: MIT License',   # Again, pick a license
+    'Programming Language :: Python :: 3.6',      #Specify which pyhton versions that you want to support
+  ],
+)
