dd34b7f1012effcf2111b2630e63a57f9b41ed70
John Weldon <john.weldon117@gmail.com>
2021-05-29 23:14:30 -0400

    updated setup.py with import setuptools, seems to fix the pypi long_description_content_type issue

--- setup.py
+++ setup.py
@@ -1,10 +1,6 @@
+import setuptools
 from distutils.core import setup
 
-#  used to pull readme.md into file
-from os import path
-this_directory = path.abspath(path.dirname(__file__))
-with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
-    long_description = f.read()
 ver = '1.0.04'
 setup(
   name='statbasket',         # How you named your package folder (MyLib)
@@ -12,20 +8,20 @@ setup(
   version=ver,      # Start with a small number and increase it with every change you make
   license='MIT',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
   description='A small statistics package for data science students and enthusiasts',   # Give a short description about your library
-  long_description=long_description,
+  long_description=open("README.md").read(),
   long_description_content_type='text/markdown',
   author='John Weldon',                   # Type in your name
   author_email='john.weldon117@gmail.com',      # Type in your E-Mail
   url='https://github.com/chumbie/statbasket',   # Provide either the link to your github or to your website
   download_url=f'https://github.com/chumbie/statbasket/archive/refs/tags/v_0{ver}.tar.gz',    # I explain this later on
   keywords=[
-      'statistics', 
-      'data-science', 
-      'students', 
-      'lite', 
+      'statistics',
+      'data-science',
+      'students',
+      'lite',
       'pure-python'],   # Keywords that define your package best
   install_requires=[],           # I get to this in a second
-      
+
   classifiers=[
     'Development Status :: 5 - Production/Stable',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
     'Intended Audience :: Developers',      # Define that your audience are developers
@@ -34,3 +30,4 @@ setup(
     'Programming Language :: Python :: 3.6',      #Specify which pyhton versions that you want to support
   ],
 )
+
