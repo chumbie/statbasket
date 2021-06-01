df6931ec17c8dc45dc508475072d3e5137e19c22
John Weldon <john.weldon117@gmail.com>
2021-06-01 12:15:51 -0400

    updated __init__.py, import classes

--- statbasket/__init__.py
+++ statbasket/__init__.py
@@ -1,2 +1,2 @@
-from statbasket.src.statmethods import StatMe
-from statbasket src.statbasket import StatBasket
+from statbasket.statmethods import StatMe
+from statbasket.statbasket.statbasket import StatBasket
